e8a4734f1668280bd6224cb03dc2b745cc684f28
Léo Stewart <leokun@me.com>
2026-05-10 17:25:55 +0200

    🔖 [config] bump version to 0.7.1

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const program = new Command();
 program
   .name("aisync")
   .description("Sync AI tool configurations between git worktrees")
-  .version("0.7.0");
+  .version("0.7.1");
 
 program
   .command("copy")
